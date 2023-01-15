90277fd24c8f1f2eab4aedaaf08516d5bc2b9cc9
kxpler442b <benmoss2002@fastmail.co.uk>
2023-01-15 18:10:52 +0000

    cleanup working tree

---
+++ src/Controllers/AuthController.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Handles user authentication and session data.
+ * 
+ * Author: B Moss
+ * Email: <P2595849@my365.dmu.ac.uk>
+ * Date: 15/01/23
+ * 
+ * @author B Moss
+ */
+
+declare(strict_types = 1);
+
+namespace App\Controllers;
+
